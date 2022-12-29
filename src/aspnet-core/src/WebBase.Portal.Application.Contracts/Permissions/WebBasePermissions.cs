ff2191a61b4c7ef3c5e544cf015d170dcfc447ff
haitc <hai.tc21@gmail.com>
2022-12-30 00:53:45 +0700

    Revert "add-portal"
    This reverts commit bc8bb1f1393038cc5254becf49d44e568ee3d619.

--- src/aspnet-core/src/WebBase.Portal.Application.Contracts/Permissions/WebBasePermissions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebBase.Permissions;
-
-public static class WebBasePermissions
-{
-    public const string GroupName = "WebBase";
-
-    //Add your own permission names. Example:
-    //public const string MyPermission1 = GroupName + ".MyPermission1";
-}

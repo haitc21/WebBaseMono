bc8bb1f1393038cc5254becf49d44e568ee3d619
haitc <hai.tc21@gmail.com>
2022-12-29 22:11:54 +0700

    add-portal

---
+++ src/aspnet-core/src/WebBase.Portal.HttpApi/Controllers/WebBaseController.cs
@@ -0,0 +1,14 @@
+﻿using WebBase.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace WebBase.Controllers;
+
+/* Inherit your controllers from this class.
+ */
+public abstract class WebBaseController : AbpControllerBase
+{
+    protected WebBaseController()
+    {
+        LocalizationResource = typeof(WebBaseResource);
+    }
+}

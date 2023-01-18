b7ce6d564b95c6b9ae963a3e29e594c0f4375138
haitc21 <hai.tc21@gmail.com>
2023-01-18 13:27:51 +0700

    add primeng to admin

--- src/angular/src/app/admin/admin.module.ts
+++ src/angular/src/app/admin/admin.module.ts
@@ -7,17 +7,54 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboardModule } from './components/dashboard/dashboard.module';
 
+import { CalendarModule } from 'primeng/calendar';
+import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
+import { BlockUIModule } from 'primeng/blockui';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { EditorModule } from 'primeng/editor';
+import { ImageModule } from 'primeng/image';
+import { BadgeModule } from 'primeng/badge';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { PickListModule } from 'primeng/picklist';
+import { KeyFilterModule } from 'primeng/keyfilter';
+
 @NgModule({
   imports: [
     AdminRoutingModule,
     ThemeModule,
     NbMenuModule,
     DashboardModule,
-    MiscellaneousModule
+    MiscellaneousModule,
+
+    CalendarModule,
+    PanelModule,
+    TableModule,
+    PaginatorModule,
+    BlockUIModule,
+    ButtonModule,
+    DropdownModule,
+    InputTextModule,
+    ProgressSpinnerModule,
+    DynamicDialogModule,
+    InputNumberModule,
+    CheckboxModule,
+    InputTextareaModule,
+    EditorModule,
+    BadgeModule,
+    ImageModule,
+    ConfirmDialogModule,
+    PickListModule,
+    KeyFilterModule,
   ],
-  declarations: [
-    AdminComponent,
-  ],
+  declarations: [AdminComponent],
 })
-export class AdminModule {
-}
+export class AdminModule {}
